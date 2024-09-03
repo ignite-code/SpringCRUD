4947f92e897beb3b8a8d144462b10e18043463a0
Shubhangi <57293871+ignite-code@users.noreply.github.com>
2024-09-03 18:11:28 +0530

    Add files via upload

---
+++ src/main/java/com/organization/IT/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.organization.IT.service;
+
+import com.organization.IT.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> findAll();
+    Employee findById(int id);
+    Employee save(Employee employee);
+    void deleteById(int id);
+}
